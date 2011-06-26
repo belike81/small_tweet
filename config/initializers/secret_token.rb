715ff17c3db64649c29ac3a2aada025d9b74f551
belike81 <belike81@gmail.com>
2011-06-26 11:18:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmallTweet::Application.config.secret_token = 'c9b665494663f54d7bb37af60f1df6dce6ea6305d30dbbd039f284fe90cd9530d99e61d525e2aeb520d2b12deef59b34a96fc4ac5c03c8bbc35cbf79698e89cb'
