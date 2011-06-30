f39c346fd995815fdb17781866adcfdc50da4f7d
belike81 <belike81@gmail.com>
2011-06-30 20:48:02 +0200

    User model validations and tests

---
+++ db/migrate/20110630184308_add_email_uniquness_index.rb
@@ -0,0 +1,11 @@
+class AddEmailUniqunessIndex < ActiveRecord::Migration
+
+  def up
+    add_index :users, :email, :unique => true
+  end
+
+  def down
+    remove_index :users, :email
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @usr = {:name => 'Test User', :email => 'test@test.org'}
+  end
+
+  it "should create a new instance when given attributes" do
+    User.create!(@usr)
+  end
+
+  it "should require name" do
+    user_no_name = User.new(@usr.merge(:name => ''))
+    user_no_name.should_not be_valid
+  end
+
+  it "should require email" do
+    user_no_email = User.new(@usr.merge(:email => ''))
+    user_no_email.should_not be_valid
+  end
+
+  it "should reject name over 50 characters" do
+    user_name = 'a' * 51
+    new_user = User.new(@usr.merge(:name => user_name))
+    new_user.should_not be_valid
+  end
+
+  it "should accept only valid email" do
+    sample_email = %w[sample.email@text.com ThIS_SAMPLE@sample.email.com another@email.com]
+    sample_email.each do |email|
+      new_user = User.new(@usr.merge(:email => email))
+      new_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email" do
+    sample_email = %w[sample.email@text,com ThIS_SAMPLE_sample.email.com another@email.]
+    sample_email.each do |email|
+      new_user = User.new(@usr.merge(:email => email))
+      new_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@usr)
+    new_user = User.new(@usr)
+    new_user.should_not be_valid
+  end
+
+  it "should mark as duplicate email addresses unless different case" do
+    upcase_email = @usr[:email].upcase
+    User.create!(@usr.merge(:email => upcase_email))
+    new_user = User.new(@usr)
+    new_user.should_not be_valid
+  end
 end
