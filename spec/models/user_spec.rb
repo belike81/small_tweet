637757233dc6ff53fff5e6a8b354feb273ae0ff4
belike81 <belike81@gmail.com>
2011-07-01 20:30:04 +0200

    Added basic password setup to user (non encrypted)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
 
   before(:each) do
-    @usr = {:name => 'Test User', :email => 'test@test.org'}
+    @usr = {
+        :name => 'Test User',
+        :email => 'test@test.org',
+        :password => 'password',
+        :password_confirmation => 'password'
+    }
   end
 
   it "should create a new instance when given attributes" do
@@ -54,4 +59,57 @@ describe User do
     new_user = User.new(@usr)
     new_user.should_not be_valid
   end
+
+  describe "passwords" do
+
+    before(:each) do
+      @user = User.new(@usr)
+    end
+
+    it "should have a password field" do
+      @user.should respond_to(:password)
+    end
+
+    it "should have a password confirmation" do
+      @user.should respond_to(:password_confirmation)
+    end
+
+    it "should require a password" do
+      User.new(@usr.merge(:password => '', :password_confirmation => '')).should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@usr.merge(:password_confirmation => 'invalid')).should_not be_valid
+    end
+
+    it "should reject short password" do
+      pass = "a" * 3
+      pass_hash = @usr.merge(:password => pass, :password_confirmation => pass)
+      User.new(pass_hash).should_not be_valid
+    end
+
+    it "should reject long password" do
+      pass = "a" * 31
+      pass_hash = @usr.merge(:password => pass, :password_confirmation => pass)
+      User.new(pass_hash).should_not be_valid
+    end
+
+    describe "password encryption" do
+
+      before(:each) do
+        @user = User.create!(@usr)
+      end
+
+      it "should have encrypted password" do
+        @user.should respond_to(:encrypted_password)
+      end
+
+      it "should set the encrypted password attribute" do
+        @user.encrypted_password.should_not be_blank
+      end
+
+    end
+
+  end
+
 end
